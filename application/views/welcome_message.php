ec495eeb135ce982dbe4c01f64b6d8c15f1f04bb
Andre Aditya P <andre02.9d@gmail.com>
2019-08-01 13:03:23 +0700

    Menambahkan section dinamis dan foto

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -73,7 +73,7 @@
 					</div>
 				</div>
 
-				
+
 			</div>
 		</div>
 		<!-- MODAL END -->
@@ -86,14 +86,17 @@
 	<div class="container">
 		<div class="row">
 			<div class="col-md-6 col-xs-12">
-				<h1>Foto Profil</h1>
+				<center>
+					<br><br>
+				<img src="https://i.ibb.co/h2Y8z7f/1564635301016.png" alt="">
+				<center>
 			</div>
 			<div class="col-md-6 col-xs-12">
 				<br><br>
 				<div class="center" id="profile">
-					<h5>Tentang Saya</h5>
-					<h2>Perkenalkan</h2>
-					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+					<h5><?php echo $header->sec_title ?></h5>
+					<h2><?php echo $header->title_perkenalan ?></h2>
+					<p>	<?php echo $header->content_perkenalan ?></p>
 				</div>
 			</div>
 			<!-- END Perkenalan -->
@@ -102,12 +105,12 @@
 		<div class="row">
 			<div class="col-md-6 col-xs-12">
 				<div class="jumbotron">
-					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+					<p><i>"<?php echo $header->content_quotes ?>"</i></p>
 				</div>
 			</div>
 			<div class="col-md-6 col-xs-12">
 				<div class="center">
-					<h1>Foto Lkmm</h1>
+					<img src="https://i.ibb.co/wW2mfth/1564636218021.png" alt="">
 				</div>
 			</div>
 		</div>
