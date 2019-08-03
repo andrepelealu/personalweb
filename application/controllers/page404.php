f05042f5b9a07d3a4146fae58f9299051190ae11
Andre Aditya P <andre02.9d@gmail.com>
2019-08-03 22:43:33 +0700

    Menambahkan Halaman notfound

--- application/controllers/page404.php
+++ application/controllers/page404.php
@@ -1,10 +1,19 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class page404 extends CI_Controller {
+class Page404 extends CI_Controller {
+  public function __construct(){
+    parent::__construct();
+    $this->load->model('homepage_model');
+    $this->load->model('hp_model');
+    $this->load->model('UserModel');
+
+  }
   public function index()
   {
-    redirect(base_url());
+    $this->load->view('template/header');
+    $this->load->view('page404_view');
+    $this->load->view('template/footer');
   }
 
 }
--- application/views/page404_view.php
+++ application/views/page404_view.php
@@ -1,1 +1,8 @@
-<h1>not found</h1>
+<section>
+  <div class="container">
+    <div class="jumbotron center">
+      <h3>Page Not Found</h3>
+      <a href="<?php echo base_url() ?>">Klik Disini Untuk Kembali ..</a>
+    </div>
+  </div>
+</section>
